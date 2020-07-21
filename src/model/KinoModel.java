6a52301ad4aae1adf2bff54f1e9838b7183dc9ef
Kjell Treder <kjell.treder@gmail.com>
2020-07-21 23:20:24 +0200

    added getter, changed file name to file path

--- src/model/KinoModel.java
+++ src/model/KinoModel.java
@@ -18,17 +18,17 @@ import model.enums.*;
 public class KinoModel {
 
     // Datafields, which change during runtime
-    public Movie chosenMovie; // set based on user input in the movie tab
-    public Showtime[] availableTimes; // set based on the chosen movie
+    private Movie chosenMovie; // set based on user input in the movie tab
+    private Showtime[] availableTimes; // set based on the chosen movie
 
-    public Showtime chosenTime; // set based on user input in the times tab
-    public Seat[][] availableSeats; // set based on the chosen time
+    private Showtime chosenTime; // set based on user input in the times tab
+    private Seat[][] availableSeats; // set based on the chosen time
 
-    public List<Seat> chosenSeats; // set based on user input in the seat tab
-    public int carSeatCount; // contains number of CarSeats in chosenSeats
-    public List<String> licensePlates; // contains inputted license plates for CarSeats
+    private List<Seat> chosenSeats; // set based on user input in the seat tab
+    private int carSeatCount; // contains number of CarSeats in chosenSeats
+    private List<String> licensePlates; // contains inputted license plates for CarSeats
 
-    public Map<Catering, Integer> chosenCatering; // set based on user input in the catering tab
+    private Map<Catering, Integer> chosenCatering; // set based on user input in the catering tab
 
     /**
      * Constructor, creates movies and caterings
@@ -44,7 +44,7 @@ public class KinoModel {
      */
     private void createMovies() {
         Movie[] f = new Movie[] { // create array with all movies
-            new Movie("The Fermentor", Genres.Action, FSKs.FSK_18, "the_fermentor", new Showtime[] {
+            new Movie("The Fermentor", Genres.Action, FSKs.FSK_18, "images/the_fermentor.jpg", new Showtime[] {
                 new Showtime(Dates.Mo, Times.PM_8, 5, 7),
                 new Showtime(Dates.Di, Times.PM_10, 4, 7),
                 new Showtime(Dates.Do, Times.PM_9_30, 5, 8),
@@ -54,7 +54,7 @@ public class KinoModel {
                 new Showtime(Dates.So, Times.PM_3, 5, 7),
                 new Showtime(Dates.So, Times.PM_5, 5, 8)
             }),
-            new Movie("Suchet Nerno", Genres.Familie, FSKs.FSK_0, "suchet_nerno", new Showtime[] {
+            new Movie("Suchet Nerno", Genres.Familie, FSKs.FSK_0, "images/suchet_nerno.jpg", new Showtime[] {
                 new Showtime(Dates.Mi, Times.PM_5, 4, 8),
                 new Showtime(Dates.Fr, Times.PM_4, 5, 7),
                 new Showtime(Dates.Fr, Times.PM_6_30, 4, 9),
@@ -62,26 +62,26 @@ public class KinoModel {
                 new Showtime(Dates.Sa, Times.PM_4_30, 5, 7),
                 new Showtime(Dates.So, Times.PM_3, 4, 9),
             }),
-            new Movie("Abduction 2", Genres.Thriller, FSKs.FSK_12, "abduction_2", new Showtime[] {
+            new Movie("Abduction 2", Genres.Thriller, FSKs.FSK_12, "images/abduction_2.jpg", new Showtime[] {
                 new Showtime(Dates.Mi, Times.PM_7, 5, 7),
                 new Showtime(Dates.Fr, Times.PM_8_30, 6, 7),
                 new Showtime(Dates.Sa, Times.PM_5, 5, 8),
                 new Showtime(Dates.Sa, Times.PM_7_30, 4, 6),
                 new Showtime(Dates.So, Times.PM_7, 5, 8)
             }),
-            new Movie("Wintertime", Genres.Familie, FSKs.FSK_6, "wintertime", new Showtime[] {
+            new Movie("Wintertime", Genres.Familie, FSKs.FSK_6, "images/wintertime.jpg", new Showtime[] {
                 new Showtime(Dates.Mo, Times.PM_5, 4, 7),
                 new Showtime(Dates.Fr, Times.PM_6_30, 5, 8),
                 new Showtime(Dates.So, Times.PM_5, 6, 7),
                 new Showtime(Dates.So, Times.PM_7, 4, 6),
                 new Showtime(Dates.So, Times.PM_7_30, 5, 6)
             }),
-            new Movie("Dueness", Genres.Horror, FSKs.FSK_18, "dueness", new Showtime[] {
+            new Movie("Dueness", Genres.Horror, FSKs.FSK_18, "images/dueness.jpg", new Showtime[] {
                 new Showtime(Dates.Mo, Times.PM_10, 5, 6),
                 new Showtime(Dates.Sa, Times.PM_9_30, 4, 9),
                 new Showtime(Dates.Sa, Times.PM_10, 5, 7)
             }),
-            new Movie("Missing Throne", Genres.Western, FSKs.FSK_12, "missing_throne", new Showtime[] {
+            new Movie("Missing Throne", Genres.Western, FSKs.FSK_12, "images/missing_throne.jpg", new Showtime[] {
                 new Showtime(Dates.Mi, Times.PM_6_30, 4, 9),
                 new Showtime(Dates.Mi, Times.PM_8, 5, 7),
                 new Showtime(Dates.Do, Times.PM_5_30, 5, 9),
@@ -89,16 +89,16 @@ public class KinoModel {
                 new Showtime(Dates.Sa, Times.PM_8, 4, 9)
             }),
             null, // test movie
-            new Movie("Corrupted Movie 1", null, FSKs.FSK_12, "yellow", new Showtime[] { // test movie
+            new Movie("Corrupted Movie 1", null, FSKs.FSK_12, "images/yellow.jpg", new Showtime[] { // test movie
                 new Showtime(Dates.Mi, Times.PM_6_30, 5, 9),
                 new Showtime(Dates.Mi, Times.PM_8, 6, 7),
                 new Showtime(Dates.Do, Times.PM_5_30, 4, 9),
                 new Showtime(Dates.Do, Times.PM_7, 5, 8),
                 new Showtime(Dates.Sa, Times.PM_8, 5, 9)
             }),
-            new Movie("Corrupted Movie 2", Genres.Western, null, "blue", null // test movie
+            new Movie("Corrupted Movie 2", Genres.Western, null, "images/does-not-exist-test.jpg", null // test movie
             ),
-            new Movie(null, Genres.Western, FSKs.FSK_12, "yellow", null // test movie
+            new Movie(null, Genres.Western, FSKs.FSK_12, "images/yellow.jpg", null // test movie
             ),
             new Movie("Corrupted Movie 4", Genres.Western, FSKs.FSK_12, null, new Showtime[] { // test movie
                 new Showtime(null, Times.PM_6_30, 4, 9),
@@ -109,11 +109,11 @@ public class KinoModel {
                 new Showtime(Dates.Sa, Times.PM_8, 0, 0),
                 new Showtime(null, null, 0, 0),
             }),
-            new Movie("Corrupted Movie 5", null, null, "green", new Showtime[] { // test movie
+            new Movie("Corrupted Movie 5", null, null, "images/green.jpg", new Showtime[] { // test movie
                 new Showtime(Dates.Fr, Times.PM_6_30, 8, 9),
             }),
             null, // test movie
-            new Movie("The Operator", Genres.Action, FSKs.FSK_16, "the_operator", new Showtime[] {
+            new Movie("The Operator", Genres.Action, FSKs.FSK_16, "images/the_operator.jpg", new Showtime[] {
                 new Showtime(Dates.Di, Times.PM_7_30, 5, 6),
                 new Showtime(Dates.Do, Times.PM_8_30, 5, 6),
                 new Showtime(Dates.Fr, Times.PM_5, 4, 6),
@@ -216,11 +216,11 @@ public class KinoModel {
             Catering equivalentCatering = Catering.ALL_CATERINGS.get(i); // get the catering at the index, which is equivalent to the index of the amount for this catering
             if (equivalentCatering == null)
                 continue; // catering is null, skip
-            if (equivalentCatering.toString() == null)
+            if (equivalentCatering.getName() == null)
                 continue; // no name set, skip
             try {
                 equivalentCatering.price.getPrice(); // try to get the price of the catering
-            } catch (Exception e) {
+            } catch (Exception ex) {
                 continue; // no price set, skip
             }
             chosenCatering.put(equivalentCatering, cateringAmounts.get(y++)); // put catering as key with the selected amount as a value in the map
@@ -236,28 +236,6 @@ public class KinoModel {
     public void setLicensePlates(List<String> lps) {
         System.out.println("DEBUG: " + "model: License plate set, License plates: " + lps); // DEBUG
         licensePlates = lps;
-    }
-
-    /**
-     * adds all prices from chosen seats and caterings together to calculate the total price
-     * @return calculated total price
-     */
-    public double calculatePrice() {
-        System.out.println("DEBUG: " + "model: calculating price..."); // DEBUG
-        double price = 0.0; // local variable, holds the price
-        if (!chosenSeats.isEmpty()) { // check, if there are chosen seats
-            for (Seat s : chosenSeats) { // check every seat
-                price += s.price.getPrice(); // add price of the seat to the total amount
-            }
-        }
-        if (chosenCatering != null) { // check, if there are chosen caterings
-            for (Map.Entry<Catering, Integer> entry : chosenCatering.entrySet()) { // check every entry of the map
-                Catering c = entry.getKey();
-                Integer i = entry.getValue();
-                price += c.price.getPrice() * i; // price is equal to the price multiplied by the amount
-            }
-        }
-        return Math.round(price * 100.0) / 100.0; // round price to two decimal places
     }
 
     /**
@@ -326,7 +304,7 @@ public class KinoModel {
             } 
             chosenTime.updateAvailability(); // update the availability of the showtime, because seats got reserved
         }
-        Order.ALL_ORDERS.add(new Order(chosenMovie, chosenTime, chosenSeats, chosenCatering, calculatePrice())); // create and add a new order with all information
+        Order.ALL_ORDERS.add(new Order(chosenMovie, chosenTime, chosenSeats, chosenCatering, getPrice())); // create and add a new order with all information
         System.out.println("\n" + "DEBUG: model: All orders are: \n" + Order.ALL_ORDERS + "\n"); // DEBUGs
         // TODO write the order to a file
     }
@@ -335,11 +313,98 @@ public class KinoModel {
      * get all the tickets as strings
      * @return array of strings with the ticket numbers
      */
-    public String[] getTicketStrings() { // TODO is this stringbuilding not better in the view?
+    public String[] getTicketStrings() {
         String print = "";
         for (Seat s : chosenSeats) { // go through the chosen seats
             if (s instanceof BeachChairSeat) print += ((BeachChairSeat)s).getTicket() + Vocabulary.SPLITTER_STRING; // get the ticket from the seat and add it to the string
         }
         return print.split(Vocabulary.SPLITTER_STRING); // split the string
+    }
+
+    /**
+     * get the total price
+     * adds all prices from chosen seats and caterings together to calculate the total price
+     * @return total price
+     */
+    public double getPrice() {
+        System.out.println("DEBUG: " + "model: calculating price..."); // DEBUG
+        double price = 0.0; // local variable, holds the price
+        if (!chosenSeats.isEmpty()) { // check, if there are chosen seats
+            for (Seat s : chosenSeats) { // check every seat
+                price += s.price.getPrice(); // add price of the seat to the total amount
+            }
+        }
+        if (chosenCatering != null) { // check, if there are chosen caterings
+            for (Map.Entry<Catering, Integer> entry : chosenCatering.entrySet()) { // check every entry of the map
+                Catering c = entry.getKey();
+                Integer i = entry.getValue();
+                price += c.price.getPrice() * i; // price is equal to the price multiplied by the amount
+            }
+        }
+        return Math.round(price * 100.0) / 100.0; // round price to two decimal places
+    }
+
+    /**
+     * get the chosen movie
+     * @return chosen movie
+     */
+    public Movie getChosenMovie() {
+        return chosenMovie;
+    }
+
+    /**
+     * get the available times
+     * @return array of showtimes
+     */
+    public Showtime[] getAvailableTimes() {
+        return availableTimes;
+    }
+
+    /**
+     * get the chosen time
+     * @return chosen time
+     */
+    public Showtime getChosenTime() {
+        return chosenTime;
+    }
+
+    /**
+     * get the available seats
+     * @return array of seats
+     */
+    public Seat[][] getAvailableSeats() {
+        return availableSeats;
+    }
+
+    /**
+     * get the chosen seats
+     * @return list of seats
+     */
+    public List<Seat> getChosenSeats() {
+        return chosenSeats;
+    }
+
+    /**
+     * get the amount of car seats
+     * @return number of car seats
+     */
+    public int getCarSeatCount() {
+        return carSeatCount;
+    }
+
+    /**
+     * get the license plates
+     * @return list of license plates
+     */
+    public List<String> getLicensePlates() {
+        return licensePlates;
+    }
+
+    /**
+     * get the caterings
+     * @return map with caterings and amounts
+     */
+    public Map<Catering, Integer> getChosenCatering() {
+        return chosenCatering;
     }
 }
