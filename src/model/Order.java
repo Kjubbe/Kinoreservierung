f052adcef38be402ba148afb7fa7a48828f9e4f3
Kjell Treder <kjell.treder@gmail.com>
2020-07-23 18:21:40 +0200

    added order number and changed toString formatting

--- src/model/Order.java
+++ src/model/Order.java
@@ -21,6 +21,7 @@ public class Order {
     private final List<Seat> seats;
     private final Map<Catering, Integer> caterings;
     private final double totalPrice;
+    private final int orderNumber; // TODO maybe change this
 
     /**
      * constructor, assigns data fields
@@ -35,6 +36,7 @@ public class Order {
         this.seats = seats;
         this.caterings = caterings;
         this.totalPrice = totalPrice;
+        this.orderNumber = ALL_ORDERS.size(); // TODO maybe change this
     }
 
     /**
@@ -43,6 +45,46 @@ public class Order {
      */
     @Override
     public String toString() {
-        return movie + ", " + time.getDateAndTime() + ", " + seats + ", " + caterings + ", " + totalPrice + Vocabulary.CURRENCY;
+        StringBuilder builder = new StringBuilder();
+        builder.append(Vocabulary.ORDER_MSGS[0] + " " + orderNumber + "\n"); // msg with order number
+        builder.append(Vocabulary.ORDER_MSGS[1] + "\n"); // second msg
+        builder.append("\n" + Vocabulary.MOVIE_LABEL + ": " + movie + "\n"); // movie
+        builder.append("\n" + Vocabulary.TIME_LABEL + ": " + time.getDateAndTime() + "\n"); // time
+        
+        // seats
+        builder.append("\n" + Vocabulary.SEATS_LABEL + ": " + "\n");
+        for (Seat s : seats) {
+            builder.append("- " + s.name + " (" + s.price.getPrice() + Vocabulary.CURRENCY + "), "); // seat
+            if (s instanceof BeachChairSeat)
+                builder.append(Vocabulary.TICKET_LABEL + ": " + ((BeachChairSeat)s).getTicket() + "\n"); // add ticket
+            else
+                builder.append(Vocabulary.LICENSE_PLATE_LABEL[0] + ": \"" + ((CarSeat)s).licensePlateNr + "\"" + "\n"); // add license plate
+        }
+
+        // catering
+        builder.append("\n" + Vocabulary.CATERING_LABEL + ": ");
+        if (caterings != null) {
+            for (Map.Entry<Catering, Integer> entry : caterings.entrySet()) { // go through every entry of the map
+                Catering c = entry.getKey();
+                Integer i = entry.getValue();
+                if (i == 0) // check if the catering is chosen, if not skip
+                    continue; // skip this entry
+                builder.append("\n" + i + "x " + c.name + " (" + Math.round(c.price.getPrice() * i * 100.0) / 100.0 + Vocabulary.CURRENCY + ")"); // add the catering name and price with their amount to the print
+            }
+        } else {
+            builder.append(Vocabulary.NONE_LABEL);
+        }
+
+        builder.append("\n" + "\n" + Vocabulary.TOTAL_PRICE_LABEL + ": " + totalPrice + Vocabulary.CURRENCY); // price
+        
+        return builder.toString();
+    }
+
+    /**
+     * get the order number for this order
+     * @return order number of this order
+     */
+    public int getOrderNumber() {
+        return orderNumber;
     }
 }
